faf27c13835829a70a7517b3b7cbf72f68828d2b
Yejin Min <68144657+psy4319@users.noreply.github.com>
2022-09-09 22:50:16 +0900

    [Bronze II] Title: 방 배정, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int grooms[7] = { };
+int brooms[7] = { };
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int n, k, s, y;
+	int cnt = 0;
+	
+	cin >> n >> k;
+	for(int i = 0; i < n; i++) {
+		cin >> s >> y;
+		if(s == 0) grooms[y]++;
+		else brooms[y]++;
+	}
+	
+	// s = 0 여학생, s = 1 남학생
+	for(int i = 1; i <= 6; i++) {
+		cnt += grooms[i] / k;
+		if(grooms[i] % k) cnt++;
+		cnt += brooms[i] / k;
+		if(brooms[i] % k) cnt++;
+	}
+	
+	cout << cnt;
+}
