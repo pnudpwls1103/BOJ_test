dad0b5cdb11d7c06b4850249266712ac1a84c82f
Yejin Min <68144657+pnudpwls1103@users.noreply.github.com>
2023-02-07 16:28:29 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,25 +1,26 @@
 def solution(id_list, report, k):
-    temp = { id : False for id in id_list }
-    check_list = { id : temp.copy() for id in id_list }
+    check_list = { id : set() for id in id_list }
     
-    # id : {신고한 id : T/F}
     for element in report:
         _from, to = element.split()
-        check_list[_from][to] = True
+        check_list[_from].add(to)
         
     count_list = { id : 0 for id in id_list }
     for _from in id_list:
         cur = check_list[_from]
-        for key, value in cur.items():
-            if value: count_list[key] += 1
+        for stop_name in cur:
+            count_list[stop_name] += 1
+
+    # check_list : { id : [신고한 사람] }
+    # count_list : { id : 신고 횟수}
+    # answer     : { id 순으로 메일 횟수 }
+    answer = []
     
-    # 여기 코드 맘에 안들어ㅓㅓㅓㅓ
-    answer = { id : 0 for id in id_list }
-    for id in id_list:
-        if count_list[id] >= k:
-            for key, value in check_list.items():
-                if value[id]:   answer[key] += 1
-    
-    answer = list(answer.values())
-    
+    for name in id_list:
+        mail = 0
+        for user in check_list[name]:
+            if count_list[user] >= k:
+                mail += 1
+        answer.append(mail)
+
     return answer
