eb3afd9ecdabc8217ef53b91f985bdd97bd2a057
Yejin Min <68144657+pnudpwls1103@users.noreply.github.com>
2024-06-05 10:42:55 +0900

    [Bronze III] Title: 공 넣기, Time: 76 ms, Memory: 5764 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.cs
+++ 백준/Bronze/10810. 공 넣기/공 넣기.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace MyCompiler {
     class Program {
@@ -9,12 +10,18 @@ namespace MyCompiler {
             var m = input.ElementAt(1);
 
             var result = new int[n+1];
+            var rows = new IEnumerable<int>[m];
             for (int i = 0; i < m; i++) {
-                var row = Console.ReadLine().Split(" ").Select(x => Int32.Parse(x));
+                rows[i] = Console.ReadLine().Split(" ").Select(x => Int32.Parse(x));
+            }
+
+            for (int i = rows.Count() - 1; i >= 0; i--) {
+                var row = rows[i];
                 var start = row.ElementAt(0);
                 var end = row.ElementAt(1);
                 var num = row.ElementAt(2);
                 for (int j = start; j <= end; j++) {
+                    if (result[j] > 0) continue;
                     result[j] = num;
                 }
             }
